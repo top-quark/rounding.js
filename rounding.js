af2aad767920e7fa840c5e5d017e4875d0dccf7e
Christopher Williams <c.c.williams@hotmail.com>
2015-11-16 08:22:14 +0000

    Fixed -0 bug when ROND_HALF_AWAY_FROM_ZERO rounded up to 0

--- rounding.js
+++ rounding.js
@@ -194,7 +194,11 @@
         if (n >= 0) {
             return _round2PlusInfinity(n, fd);
         }
-        return '-' + _round2PlusInfinity(-n, fd);
+        var ret = _round2PlusInfinity(-n, fd);
+        if (!_likeZero.test(ret)) {
+            ret = '-' + ret;
+        }
+        return ret;
     },
     // Rounding function with algorithm specified
     _round = function(n, fd, alg) {
